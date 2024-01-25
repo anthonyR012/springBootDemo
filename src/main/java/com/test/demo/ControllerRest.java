efd0e89e6a99254be4c5e06bebde6e54dc771e06
Anthony rubio <arubio@deltec.com.co>
2024-01-25 13:09:36 -0500

    Add message from application.properties

--- src/main/java/com/test/demo/ControllerRest.java
+++ src/main/java/com/test/demo/ControllerRest.java
@@ -1,15 +1,23 @@
 package com.test.demo;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import org.springframework.ui.Model;
 import lombok.extern.slf4j.Slf4j;
 
 @Controller
 @Slf4j
 public class ControllerRest {
+    @Value("${indice.hello2}")
+    private String hello2;
+
     @GetMapping("/hello")
-    public String sayHello() {
+    public String sayHello(Model model) {
+        String hello = "Hello world!";
         log.info("prueba dev pro");
+        model.addAttribute("hello",hello);
+        model.addAttribute("hello2", hello2);
         return "indice";
     }
 }
