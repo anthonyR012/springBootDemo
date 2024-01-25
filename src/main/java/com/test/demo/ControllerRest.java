86b0aa2cc03b4a51c2edf8f8d36eaabc07f0dff7
Anthony rubio <arubio@deltec.com.co>
2024-01-25 13:30:51 -0500

    Create model and share data

--- src/main/java/com/test/demo/ControllerRest.java
+++ src/main/java/com/test/demo/ControllerRest.java
@@ -3,6 +3,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.test.domein.Individual;
+
 import org.springframework.ui.Model;
 import lombok.extern.slf4j.Slf4j;
 
@@ -15,9 +17,17 @@ public class ControllerRest {
     @GetMapping("/hello")
     public String sayHello(Model model) {
         String hello = "Hello world!";
+        Individual individual = new Individual();
+        individual.setName("Anthony");
+        individual.setLastName("Rubio");
+        individual.setEmail("hello@example.com");
+        individual.setPhoneNumber("318 5 451 1");
+        individual.setAge("24");
+
         log.info("prueba dev pro");
         model.addAttribute("hello",hello);
         model.addAttribute("hello2", hello2);
+        model.addAttribute("individual", individual);
         return "indice";
     }
 }
---
+++ src/main/java/com/test/domein/Individual.java
@@ -0,0 +1,15 @@
+
+package com.test.domein;
+
+import lombok.Data;
+
+@Data
+public class Individual{
+    private String name;
+    private String lastName;
+    private String phoneNumber;
+    private String email;
+    private String age;
+
+
+}
