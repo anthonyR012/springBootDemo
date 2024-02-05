6411fefcf8e60e2a6aa9d97085224e0fdabe4dc1
Anthony rubio <arubio@deltec.com.co>
2024-02-05 17:33:30 -0500

    Advanced

--- src/main/java/com/test/demo/ControllerRest.java
+++ src/main/java/com/test/demo/ControllerRest.java
@@ -1,33 +1,40 @@
 package com.test.demo;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.test.domein.Individual;
-
+import java.util.ArrayList;
 import org.springframework.ui.Model;
 import lombok.extern.slf4j.Slf4j;
 
 @Controller
 @Slf4j
 public class ControllerRest {
-    @Value("${indice.hello2}")
-    private String hello2;
 
     @GetMapping("/hello")
     public String sayHello(Model model) {
-        String hello = "Hello world!";
         Individual individual = new Individual();
-        individual.setName("Anthony");
+        individual.setName("Anderson");
         individual.setLastName("Rubio");
         individual.setEmail("hello@example.com");
         individual.setPhoneNumber("318 5 451 1");
-        individual.setAge("24");
+        individual.setAge("26");
 
-        log.info("prueba dev pro");
-        model.addAttribute("hello",hello);
-        model.addAttribute("hello2", hello2);
+        Individual individual2 = new Individual();
+        individual2.setName("Carlos");
+        individual2.setLastName("Albertto");
+        individual2.setEmail("car@example.com");
+        individual2.setPhoneNumber("318 5 451 1");
+        individual2.setAge("26");
+
+        ArrayList<Individual> individuals = new ArrayList<Individual>();
+        // individuals.add(individual);
+        // individuals.add(individual2);
+
+        log.info("============TEST DEV RUN=============");
         model.addAttribute("individual", individual);
+        model.addAttribute("individuals", individuals);
+
         return "indice";
     }
 }
